f74f92b5e54802e93d943e60d93eb31b69d47b49
tunny17 <tvnji17@gmail.com>
2023-03-29 12:09:19 +0100

    - Completion of the Banner Component - Installation of react locomotive scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 // component imports
-import { Header } from './components'
+import { Banner, Header } from './components'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Banner />
     </div>
   )
 }
