2f36a263b481c8c9e32cd34d8123c6e043c6bd3e
tunny17 <tvnji17@gmail.com>
2023-03-31 12:03:40 +0100

    animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,43 +11,20 @@ const Header = () => {
                 <img src={Logo} alt="Company Logo" />
             </div>
             <div className="info-container  --d-flex">
-                <div className="info info1">
+                <div className="info info1" data-scroll-section>
                     <p>Mon - Sat 9.00 - 18.00</p>
                     <p>Sunday Closed</p>
                 </div>
-                <div className="info info2">
+                <div className="info info2" data-scroll-section>
                     <p>Email</p>
                     <p>contact@logistics.com</p>
                 </div>
-                <div className="info info3">
+                <div className="info info3" data-scroll-section>
                     <p>Call Us</p>
                     <p>(00) 112 365 489</p>
                 </div>
             </div>
         </div>
-        {/* made it a component itself instead */}
-        {/* <div className="navigation-container  --d-flex"  data-scroll-section>
-            <div className="navigation  --d-flex">
-                <div className="left-nav --d-flex">
-                    <ul className='--d-flex'>
-                        <li className='--active'>Home</li>
-                        <li>About</li>
-                        <li>Pages</li>
-                        <li>Project</li>
-                        <li>Contact</li>
-                    </ul>
-                </div>
-                <div className="right-nav  --d-flex">
-                    <div className="socials  --d-flex">
-                        <img src={Social1} alt="" />
-                        <img src={Social2} alt="" />
-                        <img src={Social3} alt="" />
-                        <img src={Social4} alt="" />
-                    </div>
-                    <button>Request Quote</button>
-                </div>
-            </div>
-        </div> */}
     </div>
   )
 }
